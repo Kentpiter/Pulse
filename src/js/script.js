dcd2ce64f4ec66164693a85e3b2c2af35db405ae
Kentpiter <ilya_bahtin@mail.ru>
2022-11-17 01:43:34 +0300

    complete carousel section

--- src/js/script.js
+++ src/js/script.js
@@ -2,5 +2,11 @@ const slider = tns({
     container: '.carousel__inner',
     items: 1,
     slideBy: 'page',
-    autoplay: true
+    autoplay: false,
+    controls: false,
+    nav: false,
+    autoHeight: true    
   });
+
+document.querySelector('.prev').addEventListener('click', () => slider.goTo('prev'));
+document.querySelector('.next').addEventListener('click', () => slider.goTo('next'));
