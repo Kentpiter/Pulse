9e4c3c398347a423a68717b836bd2dc8804cbf53
Kentpiter <ilya_bahtin@mail.ru>
2022-11-18 20:55:09 +0300

    add script for switch .catalog-item__content on catalog-item__list

--- src/js/script.js
+++ src/js/script.js
@@ -20,15 +20,29 @@ const slider = tns({
 document.querySelector('.prev').addEventListener('click', () => slider.goTo('prev'));
 document.querySelector('.next').addEventListener('click', () => slider.goTo('next'));
 
-//Script for tabs
 (function($) {
     $(function() {
-      
+      //Script for tabs
       $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
           .closest('div.conteiner').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
       });
+
+      //Switch content on list      
+      function toggleClass(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+          })        
+      }
+
+      toggleClass('.catalog-item__link');
+      toggleClass('.catalog-item__back');
+
       
-    });
+    });    
 })(jQuery);
