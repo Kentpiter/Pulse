6c5914d3a6207842f00e7eefc6286c4623cd2bd6
Kentpiter <ilya_bahtin@mail.ru>
2022-11-24 23:05:55 +0300

    add script for consultation and modal__close

--- src/js/script.js
+++ src/js/script.js
@@ -22,26 +22,34 @@ document.querySelector('.next').addEventListener('click', () => slider.goTo('nex
 
 (function($) {
     $(function() {
-      //Script for tabs
-      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-        $(this)
-          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.conteiner').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-      });
+         //Script for tabs
+        $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+            $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.conteiner').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+        });
 
-      //Switch content on list      
-      function toggleClass(item) {
-        $(item).each(function(i) {
-            $(this).on('click', function(e) {
-                e.preventDefault();
-                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-            })
-          })        
-      }
+        //Switch content on list      
+        function toggleClass(item) {
+            $(item).each(function(i) {
+                $(this).on('click', function(e) {
+                    e.preventDefault();
+                    $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                    $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                })
+            })        
+        }
 
-      toggleClass('.catalog-item__link');
-      toggleClass('.catalog-item__back');
+        toggleClass('.catalog-item__link');
+        toggleClass('.catalog-item__back');
+    
+        // Modal
+        $('[data-modal=consultation]').on('click',function() {
+            $('.overlay, #consultation').fadeIn('slow');
+        });
+        $('.modal__close').on('click', function() {
+            $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+        })
 
       
     });    
